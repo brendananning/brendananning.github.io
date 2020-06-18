fbf9cab94d038105851ee791378682ef880fbe54
Tim Jones <timothyjones.1@outlook.com>
2020-06-18 20:44:00 +1000

    Re-enabled parallax elements

--- files/js/script.js
+++ files/js/script.js
@@ -204,7 +204,7 @@ function setMainElements(){
 function createParallaxElements(){
   //Only render parallax, instastream and stellar on main page
   var title = document.title;
-  if(title == "Brendan Anning | Wedding Celebrant Brisbane") {
+  if(title == "Brendan Anning | Brisbane Wedding Celebrant") {
     var scene = document.getElementById('scene');
     var scene2 = document.getElementById('scene-2');
     var scene3 = document.getElementById('scene-3');
